d5a547717dc4a4055597914c1f05f459e20b819d
Mauro FJ <zona.marques@gmail.com>
2025-03-29 20:49:52 -0300

    feat: slides functional

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 const span = document.querySelector('.span');
 const btn = document.querySelector('.btn');
-const next = document.querySelector('.next');
 
-const body = document.querySelector('body')
+class Slide {
+  constructor(slider, next, prev) {
+    this.slider = document.querySelector(slider);
+    this.next = document.querySelector(next);
+    this.prev = document.querySelector(prev);
+    this.slides = this.slider.querySelectorAll('.allProj');
+    this.totalSlides = this.slides.length;
+    this.currentIndex = 1;
+  }
 
-const prev = document.querySelector('.prev');
+  centerSlide() {
+    const selectedSlide = this.slides[this.currentIndex];
+    const slideCenter =
+      selectedSlide.offsetLeft + selectedSlide.offsetWidth / 2;
+    const viewportCenter = window.innerWidth / 2;
+    const offset = viewportCenter - slideCenter;
 
-const proj2 = document.querySelector('.projeto2');
+    this.slider.style.transform = `translate3d(${offset}px, 0, 0)`;
 
-// --------------------------------------------------------------------------
-// const projetos = document.querySelectorAll('.projetos')
+    console.log(
+      `Slide ${this.currentIndex}: slideCenter=${slideCenter}, viewportCenter=${viewportCenter}, offset=${offset}`,
+    );
+  }
 
-const projeto1 = document.querySelector('.projeto1')
-const projeto2 = document.querySelector('.projeto2')
-const projeto3 = document.querySelector('.projeto3')
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.totalSlides;
+    this.centerSlide();
+  }
 
-// const projetosArray = Array.from(document.querySelectorAll('.projetos'));
+  prevSlide() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.totalSlides) % this.totalSlides;
+    this.centerSlide();
+  }
 
+  onClick() {
+    this.next.addEventListener('click', this.nextSlide.bind(this));
+    this.prev.addEventListener('click', this.prevSlide.bind(this));
+  }
+}
 
-const projetosArray = [projeto1,projeto2,projeto3]
-
-projetosArray.forEach((item,index) => {
-
-console.log(item,index)
- 
-})
-
-// --------------------------------------------------------------------------
+const slide = new Slide('.image', '.next', '.prev');
+slide.centerSlide();
+slide.onClick();
 
 function showText() {
   const textElement = this.previousElementSibling;
@@ -39,12 +57,11 @@ function showText() {
   }
 }
 
-function showPreview () {
+function showPreview() {
   const preview = document.querySelector('iframe');
 
   preview.classList.add('active');
-  this.classList.add('remove')
-
+  this.classList.add('remove');
 }
 
 // function nextSlide() {
@@ -61,33 +78,31 @@ function nextSlide() {
   const slider = document.querySelector('.image');
   // const posicao = proj2.getBoundingClientRect();
   posicao = projetosArray;
-  posicao.forEach((item,index)=> {
+  posicao.forEach((item, index) => {
     item.getBoundingClientRect();
-  })
-  console.log(posicao.left)
+  });
+  console.log(posicao.left);
 
   const larguraTela = window.innerWidth;
-  const margin = (posicao.left) - (larguraTela) / 2 + (posicao.width / 2);;
+  const margin = posicao.left - larguraTela / 2 + posicao.width / 2;
 
-  slider.style.transform = `translate3d(${-margin}px,0px,0px)`
+  slider.style.transform = `translate3d(${-margin}px,0px,0px)`;
 }
 
-function prevSlide () {
-  body.style.background = 'aqua'
+function prevSlide() {
+  body.style.background = 'aqua';
 
   projetosArray[0].classList.toggle('active');
 
   const slider = document.querySelector('.image');
-  slider.style.transform = `translate3d(0px,0px,0px)`
+  slider.style.transform = `translate3d(0px,0px,0px)`;
 
-  console.log(projetosArray[0])
-  console.log('teste')
+  console.log(projetosArray[0]);
+  console.log('teste');
 }
 
 span.addEventListener('click', showText);
 btn.addEventListener('click', showPreview);
 next.addEventListener('click', nextSlide);
 
-prev.addEventListener('click', prevSlide)
-
-
+prev.addEventListener('click', prevSlide);
